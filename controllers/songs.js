9e5592be3981694c7442f6720bb30e5afe6a9667
juanrw <juan.rw24@gmail.com>
2024-04-19 13:49:14 -0500

    create basic songs route and controller layout, import and mount route in server

---
+++ controllers/songs.js
@@ -0,0 +1,7 @@
+import { Song } from "../models/song.js";
+
+
+
+export {
+  
+}
---
+++ routes/songs.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+
+const router = Router()
+
+//router.get('/', )
+
+
+export {
+  router
+}
